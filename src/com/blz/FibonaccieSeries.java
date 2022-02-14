7a09eba5756d0404380df37707b336ef9fcb64fe
Priyanka <priyankadombale27@gmail.com>
2022-02-14 09:04:55 +0530

    1.Fibonacci Series Solution

---
+++ src/com/blz/FibonaccieSeries.java
@@ -0,0 +1,23 @@
+package com.blz;
+
+import java.util.Scanner;
+
+public class FibonaccieSeries {
+
+	public static void main(String[] args) {
+		// Initialize the variables
+		int n, a = 0, b = 0, c = 1;
+		Scanner s = new Scanner(System.in); // take input from user nth time series want.
+		System.out.print("Enter value of n:");
+		n = s.nextInt();
+		System.out.print("Fibonacci Series:");
+		for (int i = 1; i <= n; i++) {
+			a = b;
+			b = c;
+			c = a + b;
+			System.out.print(a + " ");
+
+		}
+
+	}
+}
